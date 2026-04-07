2569a894cbe7be4819b792da8d6ec44aba8dbfa7
im23b-nothelferl <nothelferl@bzz.ch>
2026-04-07 09:17:25 +0200

    hvwiodfhoi

--- app/data/cv.js
+++ app/data/cv.js
@@ -1,15 +1,15 @@
 export const cv = [
   {
-    year: "2022 - Present",
-    title: "Technical School for Computer Science",
-    institution: "State Vocational School Center, Wiesau",
+    year: "2023 - Present",
+    title: "Berufsmaturität Applikationsentwickler",
+    institution: "KSH Hottingen, Zürich",
     description: "Focus on application development, system integration, and database management. Gaining a solid foundation in software engineering principles.",
   },
   {
-    year: "2020 - 2022",
-    title: "Vocational High School",
-    institution: "Leopold-Ullstein Realschule, Fürth",
-    description: "Specialization in the technical branch, which sparked my interest in programming and technology.",
+    year: "2024 - 2025",
+    title: "Exchange year USA",
+    institution: "Belvidere North Highschool",
+    description: "Highschool year in the U.S living with a host family.",
   },
   {
     year: "2016 - 2020",
